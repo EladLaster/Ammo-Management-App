da7e95b5e5bc3dc374a5003d662256d2c077e709
LiorKirshner <liorkk7@gmail.com>
2025-08-20 16:31:32 +0300

    create demo data-base and shows user's inventory

--- src/Lior/Pages/HomePageUser.jsx
+++ src/Lior/Pages/HomePageUser.jsx
@@ -1,5 +1,18 @@
-export function HomePageUser(){
-    return(
-        <h1>Home Page User</h1>
-    )
+import { inventoryMock } from "../../inventory";
+
+function HomePageUser() {
+  return (
+    <div style={{ maxWidth: 500, margin: "40px auto", direction: "rtl" }}>
+      <h2>רשימת פריטים במלאי</h2>
+      <ul>
+        {inventoryMock.map((item) => (
+          <li key={item.item_id}>
+            <b>{item.item_name}</b> - {item.category} | כמות: {item.quantity}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default HomePageUser;
