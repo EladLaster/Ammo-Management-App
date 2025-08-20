994edd00f421a318409c86533d03672520583b69
Guy Hanan <yhananguy@gmail.com>
2025-08-20 17:10:48 +0300

    Wrap main.jsx with mantine

--- src/App3.jsx
+++ src/App3.jsx
@@ -3,14 +3,24 @@ import { useState } from 'react'
 import './App.css'
 import { FormPage } from './Guy/Pages/FormPage'
 import { LoginPage } from './Guy/Pages/LoginPage'
-
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import "@mantine/core/styles.css"; 
+import './App3.css'
 function App() {
 
 return(
   <>
-  <h2>Guy</h2>
-  <FormPage/>
-  <LoginPage/>
+    {/* <MantineProvider defaultColorScheme="dark" theme={{ primaryColor: "indigo", fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif", defaultRadius: "md",}}>
+          <Notifications position="top-right" /> */}
+          <div className="container">
+            <h2>Guy</h2>
+            <div className="build">
+            <FormPage/>
+            <LoginPage/>
+            </div>
+          </div>
+    {/* </MantineProvider> */}
   </>
   )
 }
--- src/Guy/Pages/LoginPage.jsx
+++ src/Guy/Pages/LoginPage.jsx
@@ -1,10 +1,105 @@
-import { NavBar } from "./NavBar";
+import {
+  Anchor,
+  Button,
+  Flex,
+  Checkbox,
+  Divider,
+  Group,
+  Paper,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { upperFirst, useToggle } from '@mantine/hooks';
 
-export function LoginPage(){
-    return(
-        <>
-            <h1>Login Page</h1>
-            <NavBar/>
-        </>
-    )
+export function LoginPage() {
+  const [type, toggle] = useToggle(['login', 'register']);
+  const form = useForm({
+    initialValues: {
+      email: '',
+      name: '',
+      password: '',
+      terms: false,
+    },
+
+    validate: {
+      email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
+      password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
+    },
+  });
+
+  return (
+        <Paper radius="md" p="lg" withBorder style={{ width: 400, maxWidth: '90vw', background: '#222' }}>
+         <Flex direction="column" align="center" justify="center" mb="md">
+            <Text size="lg" fw={500}>
+                ניהול תחמושת
+            </Text>
+            <Text size="sm" fw={300}>
+                מערכת צה"ל לניהול מלאי
+            </Text>
+          </Flex>
+
+
+      <Divider label={`Enter ${type} information`} labelPosition="center" my="lg" />
+
+      <form onSubmit={form.onSubmit(() => {})}>
+        <Stack>
+        <div dir="rtl">
+
+          {type === 'register' && (
+            <TextInput
+              label="שמך"
+              placeholder="השם שלך"
+              value={form.values.name}
+              onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+              radius="md"
+            />
+          )}
+          <TextInput
+            required
+            label="שם משתמש או מספר אישי"
+            placeholder="הכנס שם משתמש או מספר אישי"
+            value={form.values.email}
+            onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+            error={form.errors.email && 'Invalid email'}
+            radius="md"
+            style={{ width: '100%' }}
+          />
+
+          <PasswordInput
+            required
+            label="סיסמה"
+            placeholder="הכנס סיסמה"
+            value={form.values.password}
+            onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+            error={form.errors.password && 'Password should include at least 6 characters'}
+            radius="md"
+            mb="md"
+          />
+        
+          {type === 'register' && (
+            <Checkbox
+              label="I accept terms and conditions"
+              checked={form.values.terms}
+              onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+            />
+          )}
+          </div>
+        </Stack>
+
+        <Group justify="space-between" mt="xl">
+          <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
+            {type === 'register'
+              ? 'כבר יש לך משתמש ? התחבר'
+              : "אין לך חשבון ? הרשם"}
+          </Anchor>
+          <Button text="התחבר" type="submit" radius="xl">
+              {type === 'register' ? 'Register' : 'התחבר'}
+          </Button>
+        </Group>
+      </form>
+    </Paper>
+  );
 }
